eafb9d0d8c003b111ba1a18001687bf4132752d5
colin27091 <colinregourd@gmail.com>
2021-01-26 12:02:32 +0100

    start test

---
+++ src/main/java/com/froyo/TriviaTest.java
@@ -0,0 +1,41 @@
+package com.froyo;
+
+import static org.junit.Assert.*;
+
+public class TriviaTest {
+
+    @org.junit.Test
+    public void createRockQuestion() {
+        assertEquals(1,1);
+    }
+
+    @org.junit.Test
+    public void isPlayable() {
+        assertEquals(1,1);
+    }
+
+    @org.junit.Test
+    public void add() {
+        assertEquals(1,1);
+    }
+
+    @org.junit.Test
+    public void howManyPlayers() {
+        assertEquals(1,1);
+    }
+
+    @org.junit.Test
+    public void roll() {
+        assertEquals(1,1);
+    }
+
+    @org.junit.Test
+    public void wasCorrectlyAnswered() {
+        assertEquals(1,1);
+    }
+
+    @org.junit.Test
+    public void wrongAnswer() {
+        assertEquals(1,1);
+    }
+}
